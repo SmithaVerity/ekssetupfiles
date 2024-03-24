829122b7830dc217ec861985bd95f21fc8d43d94
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-25 01:02:44 +0530

    Update 2_installsoftware.sh

--- 2_installsoftware.sh
+++ 2_installsoftware.sh
@@ -1,7 +1,7 @@
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+sudo mkdir -p /usr/local/bin && cp ./kubectl /usr/local/bin/kubectl && export PATH=/usr/local/bin:$PATH
+echo 'export PATH=/usr/local/bin:$PATH' >> ~/.bashrc
 kubectl version
 
 curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
@@ -10,7 +10,3 @@ eksctl version
 
 curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm version 
-helm completion bash >> ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
-source <(helm completion bash)
