df43f84f57003cd5e484b66324c573175f0bd31b
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-25 00:58:08 +0530

    Update 2_installsoftware.sh

--- 2_installsoftware.sh
+++ 2_installsoftware.sh
@@ -1,7 +1,6 @@
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+
 kubectl completion bash >>  ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
