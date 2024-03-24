773eb23e40f9afdc0c255616aa2a311cf9b8feea
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-25 01:09:29 +0530

    Create 4_downloadcode.sh

---
+++ 4_downloadcode.sh
@@ -0,0 +1,7 @@
+cd ~/environment
+git clone https://github.com/aws-containers/ecsdemo-frontend.git
+git clone https://github.com/aws-containers/ecsdemo-nodejs.git
+git clone https://github.com/aws-containers/ecsdemo-crystal.git
+aws kms create-alias --alias-name alias/ekssetup --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
+export MASTER_ARN=$(aws kms describe-key --key-id alias/ekssetup --query KeyMetadata.Arn --output text)
+echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
