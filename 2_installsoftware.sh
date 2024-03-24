721fb8137c76e64a5c5cb69976ebb6150cb90437
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-25 01:03:36 +0530

    Update 2_installsoftware.sh

--- 2_installsoftware.sh
+++ 2_installsoftware.sh
@@ -1,7 +1,5 @@
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mkdir -p /usr/local/bin && cp ./kubectl /usr/local/bin/kubectl && export PATH=/usr/local/bin:$PATH
-echo 'export PATH=/usr/local/bin:$PATH' >> ~/.bashrc
 kubectl version
 
 curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
