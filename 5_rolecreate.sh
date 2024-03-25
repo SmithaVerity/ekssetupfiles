4de85eaa84b929f0298a0b6543c3873733c3ce9d
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-25 20:10:07 +0530

    Update 5_rolecreate.sh

--- 5_rolecreate.sh
+++ 5_rolecreate.sh
@@ -5,3 +5,7 @@ aws iam put-role-policy --role-name ctipsfisrole --policy-name ctipsfispolicy --
 aws iam create-role --role-name ssmpermissionsrole --assume-role-policy-document file://ssmrole-trust-policy.json
 
 aws iam attach-role-policy --role-name ssmpermissionsrole --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore
+
+aws iam create-instance-profile --instance-profile-name SSMInstanceProfile
+
+aws iam add-role-to-instance-profile --instance-profile-name SSMInstanceProfile --role-name ssmpermissionsrole
