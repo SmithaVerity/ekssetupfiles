935c3d971cb5c3b5fddfb6a149f30dfd572fda22
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-25 17:57:46 +0530

    Update 5_rolecreate.sh

--- 5_rolecreate.sh
+++ 5_rolecreate.sh
@@ -1,3 +1,7 @@
 aws iam create-role --role-name ctipsfisrole --assume-role-policy-document file://fis-role-trust-policy.json
 
 aws iam put-role-policy --role-name ctipsfisrole --policy-name ctipsfispolicy --policy-document file://fis-role-permissions-policy.json
+
+aws iam create-role --role-name ssmpermissionsrole --assume-role-policy-document file://ssmpermissions-policy.json
+
+aws iam put-role-policy --role-name ctipsfisrole --policy-name ctipsfispolicy --policy-document file://fis-role-permissions-policy.json
