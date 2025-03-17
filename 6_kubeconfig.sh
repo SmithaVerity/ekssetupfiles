a39c6047bea4f5518dd2f9af79b09d7b433a4de8
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-17 11:45:03 +0530

    Update 6_kubeconfig.sh

--- 6_kubeconfig.sh
+++ 6_kubeconfig.sh
@@ -2,5 +2,5 @@ aws eks update-kubeconfig --name ekssetup-eksctl --region ${AWS_REGION}
 STACK_NAME=$(eksctl get nodegroup --cluster ekssetup-eksctl -o json | jq -r '.[].StackName')
 ROLE_NAME=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::IAM::Role") | .PhysicalResourceId')
 echo "export ROLE_NAME=${ROLE_NAME}" | tee -a ~/.bash_profile
-eksctl create iamidentitymapping --cluster ekssetup-eksctl --arn ${rolearn} --group system:masters --username admin
+
 kubectl describe configmap -n kube-system aws-auth
